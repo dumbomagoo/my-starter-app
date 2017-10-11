3c258b8cc9b8fe8c08f9821bbd60f10f75b13734
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-10-11 16:57:39 -0400

    Added text field using component's state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.help = this.help.bind(this);
+  }
+
+  help(event){
+    console.log(`HELP`);
+    this.setState({value: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +25,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello Doug!</p>
+        <p>{`Hello ${this.state.value}`}</p>
+        <input type="text" onChange={this.help}/>
       </div>
     );
   }
