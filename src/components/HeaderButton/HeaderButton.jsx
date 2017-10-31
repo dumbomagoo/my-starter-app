a90461742af91c3f48f67bd1e3d6cd93682e2a4b
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-10-31 11:44:36 -0400

    Styling for header buttons

--- src/components/HeaderButton/HeaderButton.jsx
+++ src/components/HeaderButton/HeaderButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import './HeaderButton.css';
+
 const HeaderButton = ({ label, path }) => (
   <Link to={path}>
-    <button>{label}</button>
+    <button className={'header-button'}>{label}</button>
   </Link>
 );
 
