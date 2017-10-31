2ff04d2cd42c6a9ad3113e07a5ac59e21335143f
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-10-31 14:59:51 -0400

    Refactor to NameInputBox and prevent numbers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hello from '../Hello/Hello';
-import InputBox from '../InputBox/InputBox';
+import NameInputBox from '../NameInputBox/NameInputBox';
 import Header from '../Header/Header'
 import VisitorInfo from '../VisitorInfo/VisitorInfo';
 import './App.css';
@@ -8,7 +8,7 @@ import './App.css';
 const App = ({ changeName, visitorInfo }) => (
   <div className="App">
     <Header name={visitorInfo.name} />
-    <InputBox callMeMaybe={changeName} />
+    <NameInputBox callMeMaybe={changeName} name={visitorInfo.name} />
     <Hello name={visitorInfo.name}></Hello>
     <VisitorInfo favorites={visitorInfo.favorites} />
   </div>
---
+++ src/components/NameInputBox/NameInputBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NameInputBox.css';
+
+const NameInputBox = ({ callMeMaybe, name }) => {
+  const helpMe = event => {
+    const newValue = event.target.value.replace(/[^a-zA-Z- ',]/g, '');
+    callMeMaybe(newValue);
+  };
+
+  return (
+    <div className="help">
+      <span>{'Enter your name'}</span>
+      <input type="text" onChange={helpMe} value={name} />
+    </div>
+  );
+};
+
+NameInputBox.defaultProps = {
+  callMeMaybe: () => {},
+  name: ''
+};
+
+export default NameInputBox;
