e04eb91138b3a1528e57dde02c248b42421d9043
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-10-31 10:26:04 -0400

    Cleaned up displaying border when there are no favorites

--- src/components/VisitorInfo/VisitorInfo.jsx
+++ src/components/VisitorInfo/VisitorInfo.jsx
@@ -3,17 +3,22 @@ import './VisitorInfo.css';
 
 const VisitorInfo = ({ favorites }) => (
   <div className={'visitor-info'}>
-    {(Object.keys(favorites).length ) ? <h2>{'Your Favorites'}</h2> : null}
-    <div className={'border-magic'}>
     {
-      Object.keys(favorites).map((item, index) => (
-        <div className={'favorite-row'} key={`favorite-${index}`}>
-          <div className={'favorite-cell'}>{item}</div>
-          <div className={'favorite-cell'}>{favorites[item]}</div>
+      Object.keys(favorites).length === 0 ? null :
+      <span>
+        <h2>{'Your Favorites'}</h2>
+        <div className={'border-magic'}>
+        {
+          Object.keys(favorites).map((item, index) => (
+            <div className={'favorite-row'} key={`favorite-${index}`}>
+              <div className={'favorite-cell'}>{item}</div>
+              <div className={'favorite-cell'}>{favorites[item]}</div>
+            </div>
+          ))
+        }
         </div>
-      ))
+      </span>
     }
-    </div>
   </div>
 );
 
