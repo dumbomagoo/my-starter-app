1f886fe80d4db0f4235205cf418bb6b1730ff6df
Sarah Young <sarahyoung@libp45p-23560wp.lm.lmig.com>
2017-11-02 16:50:05 -0400

    Separated out store stuff into its own module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
-import rootReducer from './reducers';
-import getRoutes from './routes'
+import getRoutes from './routes';
+import getStores from './stores';
 import './index.css';
 
-const getNewStore = () => {
-  const middlewares = [
-    applyMiddleware(thunk)
-  ];
-  
-  const finalCreateStore = compose(
-    ...middlewares
-  )(createStore);
-
-  return finalCreateStore(rootReducer, {});
-};
-
-ReactDOM.render(<Provider store={getNewStore()}>
+ReactDOM.render(<Provider store={getStores()}>
     {getRoutes()}
   </Provider>, 
   document.getElementById('root')
